ddacb819c7e0e3e019bf638a2367ba2dc78352f0
Shihaf Ahil M <54226314+shihaf@users.noreply.github.com>
2020-08-05 11:28:53 +0530

    Add files via upload
    This program automatically creates a directory in your desktop and show the time and size. The works fine unless the path specified by me and  your system is same .

---
+++ automated_directory_creator.py
@@ -0,0 +1,17 @@
+﻿import os
+import datetime
+path="C:\\Users\\ADMIN\Desktop"
+os.chdir(path)
+try:
+    os.mkdir("automated_directory")
+except:
+    pass
+tm=os.path.getmtime("automated_directory")
+size=os.path.getsize("automated_directory")
+tm_str=datetime.datetime.fromtimestamp(tm)
+print("successfully created")
+print("time modified: {}".format(tm_str))
+print("directory size: {}".format(size))
+
+
+
